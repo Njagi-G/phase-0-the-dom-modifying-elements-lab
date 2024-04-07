a5754a05dd5afefaadccec39da3ca48eb01492eb
Njagi Githaiga <njagigithaiga@gmail.com>
2024-04-08 02:04:49 +0300

    Submitting modifying-elements-lab

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Write your code here!
+
+//Instruction 1
+
+main.remove();
+
+//Instruction 2
+
+const newHeader = document.createElement("h1");
+
+//Instruction 3
+
+newHeader.id = "victory";
+
+//Instruction 4
+
+newHeader.textContent = "Dennis Githaiga is the champion";
